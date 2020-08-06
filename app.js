d796581a438224af30d22ba82c5eb1ecc1599c73
Pedro Dimas <p.dimas@campus.fct.unl.pt>
2020-08-06 15:00:01 +0100

    changed IDE and homepage

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 app.get('/',(req,res) => {
 
-    res.send("Hello World!");
+    res.send("Home Page");
 
 });
 
 const port = process.env.port || 8080;
 app.listen(port,()=>{
 
-    console.log("Hello");
+    console.log("Hello!");
 
 });
 
